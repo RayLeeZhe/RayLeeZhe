3cbcc13058b5737d0b3462f544c514eaa8672022
RayLeeZhe <rayleezheyen@gmail.com>
2023-07-14 20:47:53 +0800

    and another one

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -13,7 +13,7 @@ class LoadingBar{
 		this.domElement.style.justifyContent = 'center';
 		this.domElement.style.zIndex = '1111';
 		const barBase = document.createElement("div");
-		barBase.style.background = '#7c1ae2a';
+		barBase.style.background = '#7c1ae2';
 		barBase.style.width = '50%';
 		barBase.style.minWidth = '250px';
 		barBase.style.borderRadius = '10px';
